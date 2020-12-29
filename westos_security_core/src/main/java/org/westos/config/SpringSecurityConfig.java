6dd391711edc20c8e64705939b25b3748895c9e0
shen <shenpeiqing168@163.com>
2020-12-29 14:14:14 +0800

    httpBasic 弹出框的认证方式

---
+++ westos_security_core/src/main/java/org/westos/config/SpringSecurityConfig.java
@@ -0,0 +1,38 @@
+package org.westos.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @Author: ShenMouMou
+ * @Company:西部开源教育科技有限公司
+ * @Description:简简单单，只为教育。
+ */
+@Configuration
+@EnableWebSecurity //开启认证的过滤器链
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    //认证管理器
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //对密码进行加密
+        String pwd = passwordEncoder().encode("123");
+        auth.inMemoryAuthentication().withUser("lisi").password(pwd).authorities("admin");
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().and().authorizeRequests().anyRequest().authenticated();
+    }
+}
